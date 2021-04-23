7c877ea3ae3ef5b551ed383e2512dacb5b637d46
Imad <imad.hashmi@toptal.com>
2021-04-23 16:43:02 +1000

    added service test cases

---
+++ src/services/__tests__/security-group-service.spec.js
@@ -0,0 +1,49 @@
+const AWSmock = require('aws-sdk-mock');
+const SecurityGroupService = require('../security-group-service');
+
+describe("security-group-service", () => {
+  describe('success', () => {
+    let awsResult = [
+      { GroupId: 'group-1', GroupName: 'Group 1' },
+      { GroupId: 'group-2', GroupName: 'Group 2' }
+    ];
+    let service;
+  
+    beforeEach(() => {
+      AWSmock.mock('EC2', 'describeSecurityGroups', (callback) => {
+        callback(null, { SecurityGroups: awsResult });
+      });
+      service = new SecurityGroupService({});
+    });
+
+    afterEach(() => {
+      AWSmock.restore('EC2');
+    });
+  
+    test("successfully receives groups", async () => {
+      const result = await service.getGroups();
+      const expected = [{ GroupId: 'group-1', GroupName: 'Group 1'}, { GroupId: 'group-2', GroupName: 'Group 2'}];
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('exception', () => {
+    let awsResult = [];
+    let service;
+  
+    beforeEach(() => {
+      AWSmock.mock('EC2', 'describeSecurityGroups', (callback) => {
+        callback({ message: 'NotPermitted' });
+      });
+      service = new SecurityGroupService({});
+    });
+
+    afterEach(() => {
+      AWSmock.restore('EC2');
+    });
+  
+    it("exception", async () => {
+      await expect(() => service.getGroups()).rejects.toHaveProperty('message', 'NotPermitted');
+    });
+  });
+});
--- src/services/security-group-service.js
+++ src/services/security-group-service.js
@@ -5,12 +5,13 @@ const LogService = require('./log-service');
 class SecurityGroupService {
   constructor ({ log, ec2 }) {
     this.log = log || new LogService();
-    this.ec2 = new AWS.EC2();
+    this.ec2 = ec2 || new AWS.EC2();
   }
 
   async getGroups() {
-    const { SecurityGroups } = await this.ec2.describeSecurityGroups().promise();
-    return SecurityGroups;
+    const result = await this.ec2.describeSecurityGroups().promise();
+    const { SecurityGroups } = result;
+    return SecurityGroups;  
   }
 }
 
